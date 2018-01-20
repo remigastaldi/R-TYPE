46a589eebfe0df0d2df91bca492959e183454cbe
Remi Gastaldi <remi.gastaldi@epitech.eu>
2018-01-20 02:52:13 +0100

    remove git temp build

--- git-temp-build/libs/Alfred/test/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gastal_r/Projects/R-TYPE/libs/Alfred/test/googletest
-# Build directory: /home/gastal_r/Projects/R-TYPE/git-temp-build/libs/Alfred/test/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
