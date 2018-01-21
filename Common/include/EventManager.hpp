8e6a3530f9188d917dab864d6b2200cf12a04bb8
Dimitri Wyzlic <dimitriwyzlic@gmail.com>
2018-01-21 18:01:46 +0100

    remove mutex from eventManager

--- Common/include/EventManager.hpp
+++ Common/include/EventManager.hpp
@@ -9,7 +9,6 @@
  #include <string>
  #include <functional>
  #include <vector>
- #include <mutex>
  #include <unordered_map>
  #include "AlfredBase/EventManager/EventManagerExceptions.hpp"
 
@@ -61,53 +60,41 @@
 
      class Manager
      {
-         std::mutex _eventsMutex;
          std::unordered_map<std::string, ___event_helper *> _events;
 
        public:
          template <typename Ret, typename ...Params>
          void addEvent(const std::string &name)
          {
-             _eventsMutex.lock();
              if (_events.count(name) > 0) {
-                 _eventsMutex.unlock();
                  throw EventNameExist(name);
              }
              _events[name] = new ___Event<Ret, Params...>();
-             _eventsMutex.unlock();
          }
 
          void deleteEvent(const std::string &name)
          {
-             _eventsMutex.lock();
              _events.erase(name);
-             _eventsMutex.unlock();
          }
 
          void clear()
          {
-             _eventsMutex.lock();
              _events.clear();
-             _eventsMutex.unlock();
          }
 
          template <typename Ret, typename ...Args>
          typename std::conditional<std::is_same<Ret, void>::value, void, std::vector<Ret>>::type
          fire(const std::string &name, const Args... args)
          {
-             _eventsMutex.lock();
              if (_events.count(name) <= 0) {
-                 _eventsMutex.unlock();
                  throw EventDontExist(name);
              }
 
              if constexpr (std::is_same<Ret, void>::value) {
                  static_cast<___Event<Ret, Args...> *>(_events[name])->execute(args...);
-                 _eventsMutex.unlock();
                  return;
              } else {
                  std::vector<Ret> ret = static_cast<___Event<Ret, Args...> *>(_events[name])->execute(args...);
-                 _eventsMutex.unlock();
                  return ret;
              }
          }
@@ -115,22 +102,17 @@
          template <typename Ret, typename ...Params, typename Fctor>
          const EventListener listen(const std::string &name, const Fctor &func)
          {
-             _eventsMutex.lock();
              if (_events.count(name) <= 0) {
-                 _eventsMutex.unlock();
                  throw EventDontExist(name);
              }
              EventListener ret = static_cast<___Event<Ret, Params...> *>(_events[name])->addWatchers(func);
-             _eventsMutex.unlock();
              return ret;
          }
 
          template <typename Ret, typename ...Args>
          void unlisten(const std::string &name, const EventListener listenerID)
          {
-             _eventsMutex.lock();
              static_cast<___Event<Ret, Args...> *>(_events[name])->deleteWatcher(listenerID);
-             _eventsMutex.unlock();
          };
      };
  }
